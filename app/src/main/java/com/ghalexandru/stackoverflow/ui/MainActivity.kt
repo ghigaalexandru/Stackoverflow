24aba60727776a3076e4eaab16ef2318f174a0c0
Alexandru <alexandru.ghiga@expleogroup.com>
2021-05-04 10:17:52 +0300

    Created a basic package structure

--- app/src/main/java/com/ghalexandru/stackoverflow/ui/MainActivity.kt
+++ app/src/main/java/com/ghalexandru/stackoverflow/ui/MainActivity.kt
@@ -1,7 +1,12 @@
-package com.ghalexandru.stackoverflow
+/*
+ * Copyright (c) 2021. Parrot Faurecia Automotive S.A.S. All rights reserved.
+ */
+
+package com.ghalexandru.stackoverflow.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ghalexandru.stackoverflow.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/ghalexandru/stackoverflow/ui/Placeholder.kt
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2021. Parrot Faurecia Automotive S.A.S. All rights reserved.
+ */
+
+package com.ghalexandru.stackoverflow.ui
+
+
+class Placeholder {
+}
